8a13fd60e86ca3d89763328a98a2ed8442e817cf
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-11 09:51:54 +0200

    Création d'un composant HELLO

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Hello extends Component{
+  render(){
+    return <div>
+        <h1>Hello {this.props.prenom}</h1>
+        <p>{this.props.children}</p>
+      </div>;
+  }
+}
+
+
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Salut
+        <Hello prenom="Guillaume">
+           Enseignant <b>formidable</b> de l'IPI
+        </Hello>
+        <Hello prenom="Estelle"></Hello>
+        <Hello prenom="Corentin"></Hello>
       </div>
     );
   }
