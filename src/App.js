66ab6df1464ce5954264f7b26edaee4d19be29f5
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-11 10:23:12 +0200

    Evenement et Etat

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class Hello extends Component{
+  constructor(){
+    super();
+    this.state = {visible : false};
+  }
+
+  modif(){
+    var display = this.state.visible;
+    this.setState({visible : !display});
+  }
+
   render(){
+    if (this.state.visible)
+    var p = <p>{this.props.children}</p>;
+
     return <div>
-        <h1>Hello {this.props.prenom}</h1>
-        <p>{this.props.children}</p>
+        <h1 onClick={()=>this.modif()} >Hello {this.props.prenom}</h1>
+        {p}
       </div>;
   }
 }
