c71bf978439dc337f95904dea7c376a05e829475
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-11 12:31:54 +0200

    Ajout du service de Meteo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import './Meteo.css';
 
+var images = require.context('./images', true); 
+
 class Meteo extends Component{
 
   constructor(){
@@ -11,10 +13,32 @@ class Meteo extends Component{
     this.state = {};
     this.state.ville = "Lyon";
     this.state.conf = false;
+    this.state.min = null;
+    this.state.max = null;
+    this.state.logo = logo;
+  }
+
+  componentDidMount(){
+    this.loadMeteo();
   }
 
   loadMeteo(){
-    
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+this.state.ville+"&appid=fc4347cedde9c8fe14fffac34eed3c60")
+      .then((r)=> r.json())
+      .then((data)=> this.meteoLoaded(data));
+  }
+
+  meteoLoaded(data){
+      var min = data.main.temp_min;
+      var max = data.main.temp_max;
+      var icon = data.weather[0].icon;
+      this.setState({"min" : Math.round(min-273.15), 
+                     "max" : Math.round(max - 273.15),
+                     "logo" : images("./"+icon+".png")
+                    });
+      
+      this.setState({conf : false});
+      
   }
 
 
@@ -33,11 +57,11 @@ class Meteo extends Component{
               
               
               <div style={style} >
-                  {this.state.conf ? null : <h2 onClick={()=> this.setState({conf : true})}>Lyon</h2>}
-                  <img src={logo}></img>
+                  {this.state.conf ? null : <h2 onClick={()=> this.setState({conf : true})}>{this.state.ville}</h2>}
+                  <img src={this.state.logo}></img>
                   <ul>
-                    <li>Temperature Min : </li>
-                    <li>Temperature Max : </li>
+                    <li>Min : {this.state.min} °C</li>
+                    <li>Max : {this.state.max} °C</li>
                   </ul>
               </div>
             </div>;
