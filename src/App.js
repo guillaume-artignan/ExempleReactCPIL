e423f266dead231c5afa4f36a45b302c16edcb80
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-11 12:00:56 +0200

    Exemple de Composant de Meteo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Meteo.css';
+
+class Meteo extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {};
+    this.state.ville = "Lyon";
+    this.state.conf = false;
+  }
+
+  loadMeteo(){
+    
+  }
+
+
+  render(){
+    var style = {opacity : 1};
+    if (this.state.conf)
+    style = {opacity : 0.2};
+
+    return <div className="Meteo">
+              {this.state.conf ?  <div>
+                                    <input  onChange = {(e)=> this.setState({"ville":e.target.value})}
+                                            value={this.state.ville}></input>
+                                    <button onClick={()=>this.loadMeteo()}>Configure</button>
+                                  </div>
+                                  :null}
+              
+              
+              <div style={style} >
+                  {this.state.conf ? null : <h2 onClick={()=> this.setState({conf : true})}>Lyon</h2>}
+                  <img src={logo}></img>
+                  <ul>
+                    <li>Temperature Min : </li>
+                    <li>Temperature Max : </li>
+                  </ul>
+              </div>
+            </div>;
+  }
+}
+
 
 class Hello extends Component{
   constructor(){
     super();
     this.state = {visible : false};
+  }
+
+  componentDidMount(){
+    console.log("DID MOUNT");
   }
 
   modif(){
@@ -39,8 +86,9 @@ class App extends Component {
     return (
       <div className="App">
         {
-          this.state.tab.map((item,i)=> <Hello prenom={item.prenom}>{item.description}</Hello>)
+          this.state.tab.map((item,i)=> <Hello key={i} prenom={item.prenom}>{item.description}</Hello>)
         }
+        <Meteo></Meteo>
       </div>
     );
   }
